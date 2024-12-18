3eb645f0c449c01e98b5ab8d1f2f3dd0a0fd4da6
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2024-12-18 23:11:27 +0100

    Blocking blinky
    using a timer, and blocking on it.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,51 @@ use defmt_rtt as _;
 
 use panic_probe as _;
 
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+
+use rp2040_hal::{Timer, pac::Peripherals};
+
 #[unsafe(link_section = ".boot2")]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
 #[rp2040_hal::entry]
 fn main() -> ! {
     println!("hi!");
-    loop {}
+    let mut pac = Peripherals::take().unwrap();
+
+    let mut watchdog = rp2040_hal::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = rp2040_hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+    let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+
+    let sio = rp2040_hal::Sio::new(pac.SIO);
+
+    let pins = rp2040_hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut led = pins.gpio25.into_push_pull_output();
+
+    loop {
+        println!("loop");
+        led.set_high().unwrap();
+        timer.delay_ms(500);
+        led.set_low().unwrap();
+        timer.delay_ms(500);
+    }
 }
